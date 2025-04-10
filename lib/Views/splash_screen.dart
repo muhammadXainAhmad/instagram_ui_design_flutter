6cc919202d098c37c9da74bf48377e4b86f343bd
muhammadXainAhmad <xain2002@gmail.com>
2025-04-11 03:04:18 +0500

    Final tweaks

--- lib/Views/splash_screen.dart
+++ lib/Views/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_ui_design/Views/constants.dart';
 import 'package:instagram_ui_design/Views/home.dart';
